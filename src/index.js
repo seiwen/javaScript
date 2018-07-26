ecf22b695f1bf8636080624b098f7b0d88d37f98
王立峰 <wanglifeng@globalegrow.com>
2018-07-26 09:54:49 +0800

    '6.3'

--- src/index.js
+++ src/index.js
@@ -262,7 +262,8 @@
 // // console.log(callSum(10,10));
 
 // //apply() call() bind()
-
+//apply():调用一个对象的一个方法，用另一个对象替换当前对象。例如：B.apply(A, arguments);即A对象应用B对象的方法
+//call():调用一个对象的一个方法，用另一个对象替换当前对象。例如：B.call(A, args1, args2);即A对象调用B对象的方法
 // //charAt() charCodeAt()
 // var srtingValue = 'hello world';
 // console.log(srtingValue.charAt(1));
