232e7f099a659a499ad0087e133ff7559ca57549
王立峰 <wanglifeng@globalegrow.com>
2018-08-01 20:11:48 +0800

    7.4

--- src/index.js
+++ src/index.js
@@ -800,3 +800,18 @@ function MyObject() {
     }
 }
 //7.4.1 静态私有变量
+(function () {
+    //私有变量和私有函数
+    var privateVariable = 10;
+    function privateFunction() {
+        return false;
+    }
+    //构造函数
+    MyObject = function () {
+    };
+    //公有/特权方法
+    MyObject.prototype.publicMethod = function () {
+        privateVariable++;
+        return privateFunction();
+    };
+})();
